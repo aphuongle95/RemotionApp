ab0a8ea087d2477ecf1e16e8626f3f5f14d928c0
aphuongle95 <aphuongle95@gmail.com>
2023-08-30 00:33:23 +0200

    add undo redo button

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -1,22 +1,14 @@
 import {
 	AbsoluteFill,
-	interpolate,
-	spring,
 	useCurrentFrame,
 	useVideoConfig,
 } from 'remotion';
-import React, {useMemo} from 'react';
+import React from 'react';
 import {loadFont} from '@remotion/google-fonts/Roboto';
 import Draggable from "react-draggable";
-
+import { Input } from '@mui/material';
 
 const {fontFamily} = loadFont();
-
-const title: React.CSSProperties = {
-	fontFamily,
-	fontSize: 80,
-	fontWeight: 'bold',
-};
 
 const text: React.CSSProperties = {
 	fontWeight: 'bold',
@@ -25,67 +17,28 @@ const text: React.CSSProperties = {
 	color: '#4290F5',
 };
 
-const disappearBeforeEnd = 20;
 
 export const Overlay: React.FC = () => {
-	const frame = useCurrentFrame();
-	const {fps, durationInFrames} = useVideoConfig();
-
-	const scale = spring({
-		fps,
-		frame,
-		config: {
-			mass: 0.5,
-		},
-	});
-
-	const out = spring({
-		fps,
-		frame: frame - durationInFrames + disappearBeforeEnd,
-		config: {
-			damping: 200,
-		},
-		durationInFrames: disappearBeforeEnd,
-	});
-
-	const rotate = interpolate(out, [0, 1], [0, -Math.PI / 20]);
-	const outY = interpolate(out, [0, 1], [0, -500]);
-
-	const container: React.CSSProperties = useMemo(() => {
-		return {
-			position: 'absolute',
-			backgroundColor: 'white',
-			borderRadius: 25,
-			right: 90,
-			top: 90,
-			scale: String(scale),
-			translate: `0 ${outY}px`,
-			rotate: `${rotate}rad`,
-			padding: 40,
-		};
-	}, [scale, outY, rotate]);
 
 	const draggableContainer: React.CSSProperties = {
 		backgroundColor: 'white',
 		borderRadius: 25,
 		right: 200,
-		top: 200,
-		padding: 40
+		top: 100,
+		padding: 40,
+		width: 500,
+		height: 100,
+		alignSelf: 'center',
+		position: 'relative'
 	}
 	return (
 			<AbsoluteFill>
-				<div style={container}>
-					<div style={title}>Look</div>
-					<div style={text}>I'm an overlay!</div>
-				</div>
 				<Draggable>
 					<div style={draggableContainer}>
-						<div style={title}>Look</div>
-						<div style={text}>I can now be moved around!</div>
+					<Input style={text} defaultValue="Move me or Drag me" />
 					</div>
 				</Draggable>
 			</AbsoluteFill>
-			
-		
+
 	);
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,17 +1,40 @@
 import {Composition} from 'remotion';
 import {Overlay} from './Overlay';
+import { Button } from '@mui/material';
+
+const buttonContainer: React.CSSProperties = {
+	flexDirection: 'row',
+	alignSelf: 'center',
+	position: 'relative'
+}
+
+const buttonLeft: React.CSSProperties = {
+	color: 'orange',
+	background: 'white'
+}
+
+const buttonRight: React.CSSProperties = {
+	color: 'purple',
+	background: 'white'
+}
 
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
+			<div style={buttonContainer}>
+				<Button style={buttonLeft} variant="outlined">Undo</Button>
+				<Button style={buttonRight} variant="outlined">Redo</Button>
+			</div>
+			
 			<Composition
 				id="Overlay"
 				component={Overlay}
-				durationInFrames={75}
+				durationInFrames={10}
 				fps={30}
 				width={1920}
 				height={1080}
 			/>
+			
 		</>
 	);
 };
